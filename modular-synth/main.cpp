46bb5de415abb1c4b77e0ef26cd2c527b6227dcf
Thanh Do <thanh.do@digia.com>
2019-09-29 19:16:29 +0300

    Initial Commit

---
+++ modular-synth/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  modular-synth
+//
+//  Created by Do Thanh on 29/09/2019.
+//  Copyright © 2019 Do Thanh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
