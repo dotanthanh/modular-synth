8a365a6ef4eaadffba045c22301427166581d483
Thanh Do <thanh.do@digia.com>
2019-10-19 10:17:02 +0300

    realtime processing in main, write to wave file for testing
    Signed-off-by: Thanh Do <thanh.do@digia.com>

---
+++ modular-synth/WaveFile.cpp
@@ -0,0 +1,122 @@
+//
+//  WaveFile.cpp
+//  modular-synth
+//
+//  Created by Do Thanh on 12/10/2019.
+//  Copyright © 2019 Do Thanh. All rights reserved.
+//
+
+#include <stdio.h>
+#include <string>
+#include <fstream>
+
+
+/*
+ network-byte-order: big endian
+ host-byte-order: little endian
+*/
+//int get_endianness() {
+//    // return 0 for big-endian system
+//    int one = 1;
+//    char *p = (char *) &one;
+//    return *p;
+//}
+
+typedef struct WAVE_HEADER {
+    // RIFF chunk descriptor (12 bytes)
+    char RIFF[4]; // big endian
+    uint32_t ChunkSize;
+    char WAVE[4]; // big endian
+
+    // fmt sub-chunk (24 bytes)
+    char FmtChunkID[4]; // big endian
+    uint32_t FmtChunkSize; // 16 for PCM
+    uint16_t AudioFormat;
+    uint16_t NumOfChannels;
+    uint32_t SampleRate;
+    uint32_t ByteRate;
+    uint16_t BlockAlign; // number of bytes for 1 sample (including all channels)
+    uint16_t BitsPerSample; // 8-bit, 16-bit, 32-bit,...
+    
+    // data sub-chunk (8 bytes + ...)
+    char DataChunkID[4]; // big endian
+    uint32_t DataChunkSize;
+} wav_header;
+
+template<typename T>
+struct WaveFile {
+    wav_header header;
+    std::ofstream f;
+    uint32_t numOfSamples = 0;
+    float duration = 0.f;
+
+    // default to PCM audio format for now
+    WaveFile(std::string filePath, uint16_t numOfChannels, uint32_t sampleRate, uint32_t bitRate) {
+        std::strncpy(header.RIFF, "RIFF", 4);
+        header.ChunkSize = 36;
+        std::strncpy(header.WAVE, "WAVE", 4);
+
+        std::strncpy(header.FmtChunkID, "fmt ", 4);
+        header.SampleRate = sampleRate;
+        header.NumOfChannels = numOfChannels;
+        header.FmtChunkSize = 16;
+        header.AudioFormat = 1;
+        header.ByteRate = numOfChannels * sampleRate * bitRate / 8;
+        header.BlockAlign = numOfChannels * bitRate / 8;
+        header.BitsPerSample = bitRate;
+
+        std::strncpy(header.DataChunkID, "data", 4);
+        header.DataChunkSize = 0;
+        
+        f.open(filePath, std::ios::binary);
+        writeHeader();
+    }
+    
+    ~WaveFile() {
+        if (f.is_open()) {
+            f.close();
+        }
+    }
+    
+    void writeHeader() {
+        if (f.is_open()) {
+            f.write((char*)&header.RIFF, sizeof(header.RIFF));
+            f.write((char*)&header.ChunkSize, sizeof(header.ChunkSize));
+            f.write((char*)&header.WAVE, sizeof(header.WAVE));
+
+            f.write((char*)&header.FmtChunkID, sizeof(header.FmtChunkID));
+            f.write((char*)&header.FmtChunkSize, sizeof(header.FmtChunkSize));
+            f.write((char*)&header.AudioFormat, sizeof(header.AudioFormat));
+            f.write(reinterpret_cast<char*>(&header.NumOfChannels), sizeof(header.NumOfChannels));
+            f.write(reinterpret_cast<char*>(&header.SampleRate), sizeof(header.SampleRate));
+            f.write((char*)&header.ByteRate, sizeof(header.ByteRate));
+            f.write((char*)&header.BlockAlign, sizeof(header.BlockAlign));
+            f.write((char*)&header.BitsPerSample, sizeof(header.BitsPerSample));
+            
+            f.write((char*)&header.DataChunkID, sizeof(header.DataChunkID));
+            f.write((char*)&header.DataChunkSize, sizeof(header.DataChunkSize));
+        }
+    }
+    
+    void writeData(int32_t sample) {
+        if (f.is_open()) {
+            for (int i = 0; i < header.NumOfChannels; i++) {
+                f.write((char*)&sample, sizeof(int32_t));
+            }
+            numOfSamples++;
+        }
+    }
+    
+    void finishWrite() {
+        if (f.is_open()) {
+            uint32_t dataSize = numOfSamples * header.NumOfChannels * header.BitsPerSample / 8;
+            header.ChunkSize += dataSize;
+            header.DataChunkSize += dataSize;
+            f.seekp(4, std::ios_base::beg);
+            f.write((char*)&header.ChunkSize, sizeof(header.ChunkSize));
+            f.seekp(40, std::ios_base::beg);
+            f.write((char*)&header.DataChunkSize, sizeof(header.DataChunkSize));
+            f.close();
+        }
+    }
+};
--- modular-synth/main.cpp
+++ modular-synth/main.cpp
@@ -7,9 +7,81 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <random>
+#include <thread>
+#include <chrono>
+#include <math.h>
+#include "RingBuffer.hpp"
+#include "VCO.cpp"
+#include "VCA.cpp"
+#include "Cable.hpp"
+#include <vector>
+#include "Module.hpp"
+#include "Audio.cpp"
+#include "WaveFile.cpp"
+
+const float SRATE = 44100.f;
+const float SAMPLE_TIME = 1.f / SRATE;
+
+Module::Module() {};
+Module::~Module() {};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::vector<Module*> modules;
+    std::vector<Cable*> cables;
+
+    // test patch: VCO -> VCA
+    VCO* vco = new VCO();
+    VCA* vca = new VCA();
+    Cable* cable = new Cable();
+    cable->inputId = VCO::SINE;
+    cable->outputId = VCA::IN_1;
+    cable->inputModule = vco;
+    cable->outputModule = vca;
+
+    modules.push_back(vco);
+    modules.push_back(vca);
+    cables.push_back(cable);
+
+    ProcessArgs args;
+    args.sampleRate = SRATE;
+    args.sampleTime = SAMPLE_TIME;
+    WaveFile<int32_t> f ("/Users/thdo/Downloads/test.wav", 2, 44100, 32);
+
+    auto curTime = std::chrono::high_resolution_clock::now();
+    auto lastTime = curTime;
+    double overheadTime;
+
+    // this is for printing to file (so we can plot and test)
+    float x;
+    auto firstTime = curTime;
+    float duration;
+    while (true) {
+        for (Cable* c : cables) {
+            c->step();
+        }
+        for (Module* m : modules) {
+            m->process(args);
+        }
+        x = vca->outputs[VCA::OUT_1];
+
+        f.writeData((int32_t) (x * 1000000000));
+
+        curTime = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration<double>(curTime - firstTime).count();
+//        myfile << x << " " << duration << " " << overheadTime << std::endl;
+        
+        if (duration >= 3) {
+            break;
+        }
+        std::cout << std::endl;
+        overheadTime = std::chrono::duration<double>(curTime - lastTime).count();
+        lastTime = curTime;
+        std::this_thread::sleep_for(std::chrono::duration<double>(SAMPLE_TIME - overheadTime));
+    }
+    f.finishWrite();
+    std::cout << f.numOfSamples << std::endl;
     return 0;
 }
